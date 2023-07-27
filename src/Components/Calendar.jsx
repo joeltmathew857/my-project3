a8627b49a869c6e970d35aa7a06ee5cf15dd8c32
joel thomas mathew <joelmathew857@gmail.com>
2023-07-27 14:42:10 +0530

    added-time

--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -4,18 +4,20 @@ const CurrentDate = (props) => {
   return (
     <div>
       <p className="text-xl font-bold">The current date is: {props.date}</p>
+      <p className="text-xl font-bold">The current time is: {props.time}</p>
     </div>
   );
 };
 
 const Calendar = () => {
-  // Replace this line with your preferred method of getting the current date (e.g., using Date API).
+  // Replace this line with your preferred method of getting the current date and time (e.g., using Date API).
   const currentDate = new Date().toLocaleDateString();
+  const currentTime = new Date().toLocaleTimeString();
 
   return (
     <div className="p-8">
-      <h3 className="text-2xl font-bold mb-4">What date is it?</h3>
-      <CurrentDate date={currentDate} />
+      <h3 className="text-2xl font-bold mb-4">What date and time is it?</h3>
+      <CurrentDate date={currentDate} time={currentTime} />
     </div>
   );
 };
