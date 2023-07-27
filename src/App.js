e8a0dbbc5bce114c90b3660d2ec35956b1b9cf0e
joel thomas mathew <joelmathew857@gmail.com>
2023-07-27 22:54:20 +0530

    increment & decrement

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import Calendar from "./Components/Calendar";
 
 function App() {
-  return <Calendar />
-
-  
+  // The App component serves as the entry point of the application.
+  // It renders the Calendar component, which displays a calendar interface.
+  return <Calendar />;
 };
 
 export default App;
--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -1,30 +1,47 @@
 import React, { useState } from 'react';
 
-const MyComponent = () => {
-  // State using useState hook, initial value is false
-  const [visibility, setVisibility] = useState(false);
+const Counter = () => {
+  const [count, setCount] = useState(0);
 
-  // Define the toggleVisibility function to update the state
-  const toggleVisibility = () => {
-    setVisibility((prevVisibility) => !prevVisibility); // Toggle the visibility property
+  // Define the increment method to increase the count by 1
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  // Define the decrement method to decrease the count by 1
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  // Define the reset method to set the count back to 0
+  const reset = () => {
+    setCount(0);
   };
 
   return (
     <div className="p-4">
-      {/* Button with click handler to toggle visibility */}
+      {/* Buttons with click handlers to call the methods */}
       <button
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        onClick={toggleVisibility}
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 mr-2"
+        onClick={increment}
       >
-        Click Me
+        Increment!
       </button>
-
-      {/* Conditional rendering based on state */}
-      {visibility && (
-        <h1 className="text-2xl mt-4 font-bold">Now you see me!</h1>
-      )}
+      <button
+        className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 mr-2"
+        onClick={decrement}
+      >
+        Decrement!
+      </button>
+      <button
+        className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600"
+        onClick={reset}
+      >
+        Reset
+      </button>
+      <h1 className="mt-4">Current Count: {count}</h1>
     </div>
   );
 };
 
-export default MyComponent;
+export default Counter;
