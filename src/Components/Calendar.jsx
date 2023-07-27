31d87e7b719ffd2542a0d8db804aef078e1ce346
joel thomas mathew <joelmathew857@gmail.com>
2023-07-27 23:13:06 +0530

    Controlled Input

--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -1,47 +1,26 @@
 import React, { useState } from 'react';
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
+const ControlledInput = () => {
+  const [input, setInput] = useState('');
 
-  // Define the increment method to increase the count by 1
-  const increment = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
-
-  // Define the decrement method to decrease the count by 1
-  const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
-  };
-
-  // Define the reset method to set the count back to 0
-  const reset = () => {
-    setCount(0);
+  // Define the handleChange method to update the input state
+  const handleChange = (event) => {
+    setInput(event.target.value);
   };
 
   return (
     <div className="p-4">
-      {/* Buttons with click handlers to call the methods */}
-      <button
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 mr-2"
-        onClick={increment}
-      >
-        Increment!
-      </button>
-      <button
-        className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 mr-2"
-        onClick={decrement}
-      >
-        Decrement!
-      </button>
-      <button
-        className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600"
-        onClick={reset}
-      >
-        Reset
-      </button>
-      <h1 className="mt-4">Current Count: {count}</h1>
+      {/* Input element with value and onChange event */}
+      <input
+        type="text"
+        value={input}
+        onChange={handleChange}
+        className="px-4 py-2 border rounded outline-none"
+      />
+      <h4 className="mt-4">Controlled Input:</h4>
+      <p>{input}</p>
     </div>
   );
 };
 
-export default Counter;
+export default ControlledInput;
