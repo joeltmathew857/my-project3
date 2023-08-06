072ca6971b3a6678947083263b29ac49f15fdd58
joel thomas mathew <joelmathew857@gmail.com>
2023-08-06 22:00:21 +0530

    added-button

--- src/Components/Accoundind.jsx
+++ src/Components/Accoundind.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accoundind = ({ items }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleAccound = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>
-      {items &&
-        items.map(({ namer, title, id }) => (
-          <div className="showfiled" key={id}>
-            <div className="shoetitle">
-              <h1>{title}</h1>
-            </div>
-            <div>
-              <p className="showtitle">{namer}</p>
-            </div>
+    <div className="container flex justify-center items-center h-screen">
+      {items.map(({ namer, title, id }) => (
+        <div className="box-border w-80 p-4 border-4 mb-4 relative" key={id}>
+          <button
+            className="absolute top-0 right-0 bg-gray-300 p-1 rounded-md"
+            onClick={handleAccound}
+          >
+            +
+          </button>
+          <div className="shoetitle">
+            <h1>{title}</h1>
           </div>
-        ))}
+          <div className="showcontent">
+            {isOpen && <p className="showtitle">{namer}</p>}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
--- src/Components/accountindata.jsx
+++ src/Components/accountindata.jsx
@@ -1,20 +1,25 @@
 // accountindata.jsx
 const accountindata = [
   {
-    namer: "joel",
-    title: "lorem is good",
+    namer: "messi",
+    title: "Lionel Andrés Messi, also known as Leo Messi, is an Argentine professional footballer who plays as a forward for and captains both Major League Soccer club Inter Miami and the Argentina national team",
     id: 1,
   },
   {
-    namer: "thomas",
-    title: "bcom is good",
+    namer: "Ronaldo",
+    title: "Cristiano Ronaldo dos Santos Aveiro GOIH ComM is a Portuguese professional footballer who plays as a forward for and captains both Saudi Pro League club Al Nassr and the Portugal national team",
     id: 3,
   },
   {
-    namer: "mathew", // Corrected the property name to "namer"
-    title: "commerce is good",
+    namer: "Neymar", // Corrected the property name to "namer"
+    title: "Neymar da Silva Santos Júnior, known as Neymar Júnior or mononymously as Neymar, is a Brazilian professional footballer who plays as a forward for Ligue 1 club Paris Saint-Germain and the Brazil national team",
     id: 2,
   },
+  {
+    namer: "mbappé", // Corrected the property name to "namer"
+    title: "Kylian Mbappé Lottin is a French professional footballer who plays as a forward for Ligue 1 club Paris Saint-Germain and captains the France national team. Regarded as one of the best players in the world, ",
+    id: 2,
+  }
 ];
 
 export default accountindata;
